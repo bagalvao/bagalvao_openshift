8990542211a10c981a3fab3e84d70cddac48c97e
bagalvao <37594874+bagalvao@users.noreply.github.com>
2018-03-20 16:48:49 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ echo "<br><hr><br>";
 echo "<h1>" . $_SERVER['SERVER_ADDR'] . "</h1>"; 
 echo "<img src=http://ogcss.infoglobo.com.br/9876543210/img/oglobo.jpg>";
 
-while (1) {
- echo "aa";
-} 
+
 
 ?>
