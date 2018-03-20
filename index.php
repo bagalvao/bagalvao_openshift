e01c0e004a7fd73f6de75be6dbd0b5290fdcf412
bagalvao <37594874+bagalvao@users.noreply.github.com>
2018-03-20 16:45:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,4 +3,9 @@ echo "BAGALVAO OPENSHIFT ____";
 echo "<br><hr><br>";
 echo "<h1>" . $_SERVER['SERVER_ADDR'] . "</h1>"; 
 echo "<img src=http://ogcss.infoglobo.com.br/9876543210/img/oglobo.jpg>";
+
+while (1) {
+ echo "aa";
+} 
+
 ?>
